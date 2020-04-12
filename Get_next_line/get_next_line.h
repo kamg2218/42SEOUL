320fb0a3443279a9e75353210bb7a2c16110069a
Hyunji Yoon <hyoon@c1r3s6.42seoul.kr>
2020-04-12 18:47:53 +0900

    g

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -1,9 +1,9 @@
 #ifndef NEW_H
 # define NEW_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 typedef struct s_list
 {
@@ -11,16 +11,12 @@ typedef struct s_list
   struct s_list   *next;
 }                 t_list;
 
-int     BUFFER_SIZE;
-
-//get_next_line
 int     ft_find_i(t_list *start, int i);
 int     get_next_line(int fd, char **line);
 void    ft_add_null(t_list *lst, unsigned int i);
 void	  ft_memcpy(t_list *start, unsigned int num);
 t_list  *ft_read_file(int fd, t_list *start);
 
-//get_next_line_utils
 int     ft_start_check(t_list *start);
 void    ft_lstclear(t_list **lst);
 void    ft_lstadd_back(t_list **lst, t_list *n);
