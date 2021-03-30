5f679dbf874568c72d17d0c7ba6bee79190b0854
Hyunji Yoon <hyoon@c4r2s8.42seoul.kr>
2021-03-30 16:20:41 +0900

    update ex00

--- 11_CPP/cpp00/ex00/megaphone.cpp
+++ 11_CPP/cpp00/ex00/megaphone.cpp
@@ -8,8 +8,11 @@ int			main(int argc, char *argv[])
 	int		j;
 	int		len;
 
-	if (argc < 2)
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
+	}
 	i = 1;
 	while (argv[i])
 	{
