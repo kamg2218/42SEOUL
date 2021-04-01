8499c6193c77ceb800a6f9c6e387823839089ede
Hyunji Yoon <hyoon@c2r5s1.42seoul.kr>
2021-04-01 19:42:49 +0900

    update cpp02

--- 11_CPP/cpp02/ex01/main.cpp
+++ 11_CPP/cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@ int			main(void)
 	Fixed const	b(10);
 	Fixed const	c(42.42f);
 	Fixed const	d(b);
-	Fixed const	e(42.52f);
+	//Fixed const	e(42.52f);
 
 	a = Fixed(1234.4321f);
 
@@ -14,13 +14,13 @@ int			main(void)
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
-	std::cout << "e is " << e << std::endl;
+	//std::cout << "e is " << e << std::endl;
 	
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+	//std::cout << "e is " << e.toInt() << " as integer" << std::endl;
 	
 	return 0;
 }
