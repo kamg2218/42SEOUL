223c126a031ba9ecbe3cecebfb6c23f7c6e8bfe9
윤현지 <hyoon@student.42seoul.kr>
2021-04-12 17:05:49 +0900

    update

--- 11_CPP/cpp06/ex00/srcs/ConvertToChar.cpp
+++ 11_CPP/cpp06/ex00/srcs/ConvertToChar.cpp
@@ -23,13 +23,21 @@ ConvertToChar::~ConvertToChar()
 
 char	ConvertToChar::change(char* str)
 {
-	char*	end;
+	char*	end = NULL;
 	double	result;
 
 	result = std::strtod(str, &end);
 	if (result != result)
 		throw Nan();
-	else if (result <= 0 || result >= 127)
+	else if (end)
+		throw Impossible();
+	else if (result == 0 && strlen(str) == 1)
+	{
+		int		ch = static_cast<int>(str[0]);
+		if (ch > 31 && ch < 127)
+			result = static_cast<double>(ch);
+	}	
+	if (result <= 31 || result >= 127)
 		throw NonDisplayable();
 	return static_cast<char>(result);
 }
