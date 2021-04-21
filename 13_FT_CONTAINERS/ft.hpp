6519d5fb76362f6c88f772779ab097241aceb77b
Hyunji Yoon <hyoon@c2r9s1.42seoul.kr>
2021-04-21 21:07:09 +0900

    update

--- 13_FT_CONTAINERS/ft.hpp
+++ 13_FT_CONTAINERS/ft.hpp
@@ -27,44 +27,44 @@ namespace ft
 		//typedef bidirectionalIterator		reverse_iterator;
 		//typedef bidirectionalIterator		const_reverse_iterator;
 		//constructor
-		list() : size(0) { head = al.allocate(0); }
-		list(const list& other) { *this = other; }
-		list&	operator=(list const &lst) {
+		list(); //: size(0) { head = al.allocate(0); }
+		list(const list& other);// { *this = other; }
+		list&	operator=(list const &lst); /* {
 			size = lst.size;
 			head = al.allocate(size);
 			for (size_type i = 0; i < size; i++)
 				head[i] = lst.head[i];
 			return *this;
-		}
-		explicit list(const Allocator& alloc) : size(0) { 
+		}*/
+		explicit list(const Allocator& alloc); /* : size(0) { 
 			al = alloc;
 			head = al.allocate(0);
-		}
-		explicit list(size_type count, const T& value = T(), const Allocator& alloc = Allocator()) : size(count) {
+		}*/
+		explicit list(size_type count, const T& value = T(), const Allocator& alloc = Allocator()); /*: size(count) {
 			al = alloc;
 			head = al.allocate(count);
 			for (size_type i = 0; i < count; i++)
 				head[i] = value;
-		}
-		template<class InputIt> list(InputIt first, InputIt last, const Allocator& alloc = Allocator()) {
+		}*/
+		template<class InputIt> list(InputIt first, InputIt last, const Allocator& alloc = Allocator()); /* {
 			size = last - first;
 			//head = const_cast<Allocator&>(alloc).allocate(size);
 			al = alloc;
 			head = al.allocate(size);
 			for (InputIt i = first; i != last; i++)
 				head[i - first] = *i;
-		}
-		~list() { std::cout << "destructor\n";
-			al.deallocate(head, head[0]);}
-		void	assign(size_type count, const T& value){
+		}*/
+		~list(); /* { std::cout << "destructor\n";
+			al.deallocate(head, head[0]);} */
+		void	assign(size_type count, const T& value); /*{
 			size = count;
 			al.deallocate(head, head[0]);
 			head = al.allocate(count);
 			for (size_type i = 0; i < count; i++)
 				head[i] = value;
-		}
+		}*/
 		template<class InputIt>
-		void	assign(InputIt first, InputIt last){
+		void	assign(InputIt first, InputIt last); /*{
 			std::cout << "< last all >\n";
 			size = last - first;
 			al.deallocate(head, head[0]);
@@ -72,9 +72,9 @@ namespace ft
 			head = al.allocate(size);
 			for (size_type i = 0; i < size; i++)
 				head[i] = first++;
-		}
+		}*/
 		template<>
-		void	assign(typename std::list<T>::iterator first, typename std::list<T>::iterator last){
+		void	assign(typename std::list<T>::iterator first, typename std::list<T>::iterator last); /*{
 			std::cout << "< iterator >" << std::endl;
 			size = 0;
 			for (typename std::list<T>::iterator i = first; i != last; i++)
@@ -84,9 +84,9 @@ namespace ft
 			head = al.allocate(size);
 			for (size_type i = 0; i < size; i++, first++)
 				head[i] = *first;
-		}
+		}*/
 		template<>
-		void	assign(T* first, T* last){
+		void	assign(T* first, T* last); /*{
 			std::cout << "< pointer >\n";
 			size = last - first;
 			al.deallocate(head, head[0]);
@@ -94,7 +94,7 @@ namespace ft
 			head = al.allocate(size);
 			for (size_type i = 0; i < size; i++, first++)
 				head[i] = *first;
-		}
+		}*/
 		//allocator_type	get_allocator() const {}
 	};
 }
--- 13_FT_CONTAINERS/list.cpp
+++ 13_FT_CONTAINERS/list.cpp
@@ -1,9 +1,87 @@
 #include "ft.hpp"
-/*
+
+template <class T, class Allocator>
+template <typename InputIt>
+typename ft::list<T, Allocator>::iterator ft::list<T, al>::name()
+{
+}
+
 template <class T, class Allocator = std::allocator<T> >
-class	list {
-	typedef typename value_type 			= T;
-	typedef typename allocator_type			= Allocator;
-	typedef typename size_type				= size_t;
-	typedef typename difference_type		= ptrdiff_t;
-};*/
+{
+	ft::list::list() : size(0) { head = al.allocate(0); }
+	ft::list::list(const list& other) { *this = other; }
+
+	list&	ft::list::operator=(list const &lst) {
+		size = lst.size;
+		head = al.allocate(size);
+		for (size_type i = 0; i < size; i++)
+			head[i] = lst.head[i];
+		return *this;
+	}
+
+	explicit ft::list::list(const Allocator& alloc) : size(0) { 
+		al = alloc;
+		head = al.allocate(0);
+	}
+
+	explicit ft::list::list(size_type count, const T& value = T(), const Allocator& alloc = Allocator()) : size(count) {
+		al = alloc;
+		head = al.allocate(count);
+		for (size_type i = 0; i < count; i++)
+			head[i] = value;
+	}
+
+	template<class InputIt>
+	ft::list::list(InputIt first, InputIt last, const Allocator& alloc = Allocator()) {
+		size = last - first;
+		al = alloc;
+		head = al.allocate(size);
+		for (InputIt i = first; i != last; i++)
+			head[i - first] = *i;
+}
+
+ft::list::~list() { 
+	std::cout << "destructor\n";
+	al.deallocate(head, head[0]);
+}
+
+void	ft::list::assign(size_type count, const T& value){
+	size = count;
+	al.deallocate(head, head[0]);
+	head = al.allocate(count);
+	for (size_type i = 0; i < count; i++)
+		head[i] = value;
+}
+
+template<class InputIt>
+void	ft::list::assign(InputIt first, InputIt last){
+	std::cout << "< last all >\n";
+	size = last - first;
+	al.deallocate(head, head[0]);
+	head = al.allocate(size);
+	for (size_type i = 0; i < size; i++)
+		head[i] = first++;
+}
+
+template<>
+void	ft::list::assign(typename std::list<T>::iterator first, typename std::list<T>::iterator last){
+	std::cout << "< iterator >" << std::endl;
+	size = 0;
+	for (typename std::list<T>::iterator i = first; i != last; i++)
+		size++;
+	al.deallocate(head, head[0]);
+	head = al.allocate(size);
+	for (size_type i = 0; i < size; i++, first++)
+		head[i] = *first;
+}
+
+template<>
+void	ft::list::assign(T* first, T* last){
+	std::cout << "< pointer >\n";
+	size = last - first;
+	al.deallocate(head, head[0]);
+	head = al.allocate(size);
+	for (size_type i = 0; i < size; i++, first++)
+		head[i] = *first;
+}
+};
